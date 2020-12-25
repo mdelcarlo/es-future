3dfa5caa7ab0e5213cdbb43c2d33ddf39e2f8f1f
Matias Del Carlo <mati.dc89@gmail.com>
2020-12-26 08:11:39 +1300

    Add exponentiation operator codeblocks

---
+++ ES7/01-exponentiation-operator/codeblocks/00.js
@@ -0,0 +1,13 @@
+
+(-2) ** 2;
+// 4 in Bash, -4 in other languages.a **b
+// This is invalid in JavaScript, as the operation is ambiguous.
+
+-(2 ** 2);
+// -4 in JavaScript and the author's intention is unambiguous.
+
+2 ** 3; // 8
+3 ** 2; // 9
+3 ** 2.5; // 15.588457268119896
+10 ** -1; // 0.1
+NaN ** 2; // NaN
