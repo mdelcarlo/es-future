25b276eb1f914ca825b7ccfc50aad3b7a517952f
Matias Del Carlo <mati.dc89@gmail.com>
2020-12-27 22:01:20 +1300

    Add trailing commas codeblocks

---
+++ ES8/03-trailing-commas/codeblocks/00.js
@@ -0,0 +1,8 @@
+
+let company = {
+  name: `Netflix`, // Added by mdelcarlo with trailing comma
+  founder: ` Marc Randolph`, // Added by github_octocat with trailing comma
+  sector: `Consumer Services`, // Added by gearon with trailing comma
+};
+
+console.log(company)
